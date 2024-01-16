922e0a60a80c0be34a0cf491d7ab65729b694b8a
Chris Pennington <chris@codinginpublic.dev>
2024-01-16 13:44:11 -0700

    stage-1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn convert_temp(temp: f32) -> f32 {
+    (temp * (9.0 / 5.0)) + 32.0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_temp: f32 = 0.0;
+    let converted_temp = convert_temp(input_temp);
+    println!("Converted temp: {} degrees Fahrenheit", converted_temp);
 }
